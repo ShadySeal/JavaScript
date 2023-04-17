c9c7c1e2b7119a75436a471becff93050e4e7633
ShadySeal <122457366+ShadySeal@users.noreply.github.com>
2023-04-17 12:34:43 +0300

    Add files via upload

---
+++ JavaScript/moduuli4/t3/3.js
@@ -0,0 +1,39 @@
+'use strict';
+const input = document.querySelector('#query');
+const form = document.querySelector('form');
+const results = document.querySelector('#results');
+
+form.addEventListener('submit', async function(evt) {
+  evt.preventDefault();
+  const value_from_input = input.value;
+  try  {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${value_from_input}`);
+    const jsonData = await response.json();
+    results.innerHTML = '';
+    for (let i = 0; i < jsonData.length; i++) {
+      const tvShow = jsonData[i].show;
+      const article = document.createElement('article');
+      const h2 = document.createElement('h2');
+      const name = document.createTextNode(tvShow.name);
+      const a = document.createElement('a');
+      const url = document.createTextNode(tvShow.url);
+      const img = document.createElement('img');
+      const summary = document.createElement('div');
+      const summaryText = document.createTextNode(tvShow.summary);
+      h2.appendChild(name);
+      a.appendChild(url);
+      a.href = tvShow.url;
+      a.target = '_blank';
+      img.alt = tvShow.name;
+      img.src = tvShow.image?.medium;
+      summary.appendChild(summaryText);
+      article.appendChild(h2);
+      article.appendChild(a);
+      article.appendChild(img);
+      article.appendChild(summary);
+      results.appendChild(article);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
